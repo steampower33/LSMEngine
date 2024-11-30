9b34420f8005d0f9c425ac8faa910de5dc3951ea
steampower33 <steampower33@naver.com>
2024-12-01 07:40:20 +0900

    Add project files.

---
+++ EngineCore.cpp
@@ -0,0 +1,6 @@
+#include "EngineCore.h"
+
+namespace EngineCore
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+
+int wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd)
+{
+
+}
